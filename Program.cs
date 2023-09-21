501d47ab8bb2e08267fcacb90ed2ccaea69421b6
Andrey <gusev2097@mail.ru>
2023-09-21 19:31:41 +0500

    Task is solved

--- Program.cs
+++ Program.cs
@@ -1,20 +1,48 @@
-﻿string[] CreateArray(int length)
+﻿string[] CreateArray(int length)  // метод для заполнения массива строк
 {
     string[] array = new string [length];
     for (int i = 0; i < length; i++)
     {
-        array[i] = Console.ReadLine();
+        Console.Write($"Введите {i+1} элемент массива: ");
+        array[i] = Console.ReadLine()!;
     }
     return array;
 }
 
-bool IsThreeElement (string array)
+bool IsThreeElement (string array)  // проверка строки. Удовлетворяет ли условиям?
 {
     int count = int.Parse(array.Length.ToString());
     if (count > 3) return false;
     return true;
 }
 
+int Counter (string[] array) // считает количество строк, удовлетворяющих условиям. Для того
+{                            // чтобы создать новый массив с необходимой размерностью
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (IsThreeElement(array[i])) counter++;
+    }
+    return counter;
+}
+
+string[] CopyArrayThree (string[] array, int size) // копирует необходимые элементы в новый массив
+{
+    string[] newArray = new string [size];
+    int element = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (IsThreeElement(array[i]))
+        {
+            newArray[element] = array[i];
+            element++;
+        }
+    }
+    return newArray;
+}
+
 int length = 5;
 string[] arr = CreateArray(length);
-Console.WriteLine(IsThreeElement("tre"));
+int size = Counter(arr);
+string[] newArr = CopyArrayThree(arr, size); 
+Console.WriteLine(string.Join("; ", newArr));
